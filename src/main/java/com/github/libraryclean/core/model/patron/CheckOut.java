aee35ff49759555f9fca5a5542834fc31eab39cc
gushakov <Georgy.Ushakov@unil.ch>
2023-05-03 22:35:39 +0200

    typo

--- src/main/java/com/github/libraryclean/core/model/patron/CheckOut.java
+++ src/main/java/com/github/libraryclean/core/model/patron/CheckOut.java
@@ -4,7 +4,7 @@ import com.github.libraryclean.core.model.catalog.Isbn;
 import lombok.Value;
 
 /**
- * Lease of a {@code Book} to a {@code Partner} by a library for an agreed, fixed duration of time.
+ * Lease of a {@code Book} to a {@code Patron} by a library for an agreed, fixed duration of time.
  * A {@code CheckOut} is considered overdue, at the moment the lease period has ended and if the
  * book has not been returned to the library.
  */
