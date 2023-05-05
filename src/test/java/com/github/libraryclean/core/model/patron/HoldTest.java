a79aa183ab81352eb626f1018c7aa7ff8e2878eb
gushakov <Georgy.Ushakov@unil.ch>
2023-05-05 17:27:23 +0200

    added copyright disclaimer notice

--- src/test/java/com/github/libraryclean/core/model/patron/HoldTest.java
+++ src/test/java/com/github/libraryclean/core/model/patron/HoldTest.java
@@ -1,3 +1,16 @@
+/*
+    COPYRIGHT DISCLAIMER
+    --------------------
+
+    The code in this file may be based on the original work from
+    [ddd-by-examples/library](https://github.com/ddd-by-examples/library).
+
+    Please see the original licence at
+    https://github.com/ddd-by-examples/library/blob/master/LICENSE
+
+    and the copyright disclaimer notice in "README.md" (in this repository).
+ */
+
 package com.github.libraryclean.core.model.patron;
 
 import com.github.libraryclean.core.model.InvalidDomainObjectError;
@@ -33,5 +46,16 @@ public class HoldTest {
                 .isNotNull()
                 .isInstanceOf(InvalidDomainObjectError.class);
 
+    }
+
+
+    @Test
+    void open_ended_hold_not_completed_not_canceled_is_active() {
+        // given
+        Hold openEndedHold = Hold.of(Isbn.of("0134494164"), LocalDate.now());
+        // when
+        boolean active = openEndedHold.isActive();
+        // then
+        Assertions.assertThat(active).isTrue();
     }
 }
