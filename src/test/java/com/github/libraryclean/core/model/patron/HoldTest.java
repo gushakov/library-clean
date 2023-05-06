692bc7c88b3cba13ccfa74fc6366e3a576adc5af
gushakov <Georgy.Ushakov@unil.ch>
2023-05-06 11:57:59 +0200

    different style tests

--- src/test/java/com/github/libraryclean/core/model/patron/HoldTest.java
+++ src/test/java/com/github/libraryclean/core/model/patron/HoldTest.java
@@ -18,9 +18,9 @@ import com.github.libraryclean.core.model.catalog.Isbn;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchException;
+import static org.assertj.core.api.Assertions.*;
 
 public class HoldTest {
 
@@ -29,7 +29,7 @@ public class HoldTest {
 
         // given
         // when
-        Exception error = catchException(() -> Hold.of(null, LocalDate.now()));
+        Exception error = catchException(() -> Hold.of(null, anyDate()));
         // then
         assertThat(error)
                 .isNotNull()
@@ -42,7 +42,7 @@ public class HoldTest {
 
         // given
         // when
-        Exception error = catchException(() -> Hold.of(Isbn.of("0134494164"), null));
+        Exception error = catchException(() -> Hold.of(anyIsbn(), null));
         // then
         assertThat(error)
                 .isNotNull()
@@ -53,32 +53,56 @@ public class HoldTest {
     @Test
     void open_ended_hold_not_completed_not_canceled_is_active() {
         // given
-        Hold openEndedHold = Hold.of(Isbn.of("0134494164"), LocalDate.now());
+        Hold openEndedHold = anyOpenEndedHold();
         // when
         boolean active = openEndedHold.isActive();
         // then
         assertThat(active).isTrue();
     }
 
+    private static Hold anyOpenEndedHold() {
+        return Hold.of(anyIsbn(), anyDate());
+    }
+
+    private static Isbn anyIsbn() {
+        return Isbn.of("0134494164");
+    }
+
     @Test
     void cancel_active_hold() {
         // given
-        LocalDate fromDate = LocalDate.now();
-        Hold originalHold = Hold.of(Isbn.of("0134494164"), fromDate);
+
+        LocalDate fromDate = anyDate();
+        LocalDate laterDate = dayLater(fromDate);
+        Hold originalHold = Hold.of(anyIsbn(), fromDate);
+
         // when
-        Hold canceledHold = originalHold.cancel(fromDate.plusDays(1));
+
+        Hold canceledHold = originalHold.cancel(laterDate);
+
         // then
+
+        // original hold has not changed
         assertThat(originalHold.isActive()).isTrue();
+        assertThat(originalHold.wasCanceled()).isFalse();
+        assertThat(originalHold.wasCompleted()).isFalse();
+
+        // and
         assertThat(canceledHold.isActive()).isFalse();
         assertThat(canceledHold.wasCanceled()).isTrue();
+        assertThat(canceledHold.wasCompleted()).isFalse();
     }
 
     @Test
     void must_not_cancel_hold_with_null_cancel_date() {
         // given
-        Hold originalHold = Hold.of(Isbn.of("0134494164"), LocalDate.now());
+
+        Hold originalHold = anyOpenEndedHold();
+
         // when
+
         Exception error = catchException(() -> originalHold.cancel(null));
+
         // then
         assertThat(error)
                 .isNotNull()
@@ -88,13 +112,26 @@ public class HoldTest {
     @Test
     void must_not_cancel_hold_with_cancel_date_before_from_date() {
         // given
-        LocalDate fromDate = LocalDate.now();
-        Hold originalHold = Hold.of(Isbn.of("0134494164"), fromDate);
+        LocalDate fromDate = anyDate();
+        LocalDate dateCanceled = fromDate.minusDays(1);
+        Hold hold = Hold.of(anyIsbn(), fromDate);
+
         // when
-        Exception error = catchException(() -> originalHold.cancel(fromDate.minusDays(1)));
+        Optional<Exception> error = tryToCancel(hold, dateCanceled);
+
         // then
-        assertThat(error)
-                .isNotNull()
-                .isInstanceOf(InvalidHoldStateError.class);
+        assertThat(error).isPresent();
+    }
+
+    private static Optional<Exception> tryToCancel(Hold hold, LocalDate dateCanceled) {
+        return Optional.ofNullable(catchThrowableOfType(() -> hold.cancel(dateCanceled), InvalidHoldStateError.class));
+    }
+
+    private static LocalDate anyDate() {
+        return LocalDate.now();
+    }
+
+    private static LocalDate dayLater(LocalDate date){
+        return date.plusDays(1);
     }
 }
